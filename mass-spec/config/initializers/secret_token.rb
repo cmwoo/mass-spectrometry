6a246d12973a87a4d1b711d7f75b330f6fc65fbf
Shaina <skrevat@berkeley.edu>
2015-03-02 22:06:37 -0400

    initial commit

---
+++ mass-spec/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MassSpec::Application.config.secret_token = '61945de01e3aeac14449386b274806965b64e029afa5b9b760f9cffd73088344ba958fc7b66465ea7f7eaf90c3c0ede0f65edc1149fef73d0b1ac364f6ed2e7f'
