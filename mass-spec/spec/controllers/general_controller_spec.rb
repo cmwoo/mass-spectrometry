73d1f0a7476f79da73bb873e70d3e9d7963f6109
kerili <kerili428@gmail.com>
2015-03-13 15:11:42 -0300

    added sad path

--- mass-spec/spec/controllers/general_controller_spec.rb
+++ mass-spec/spec/controllers/general_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe GeneralController do
   before :each do
     @dataxml = fixture_file_upload('/files/test_data.xml', 'text/xml')
-    @paramsxml = fixture_file_upload('/files/test_params.txt', 'text/xml')
+    @paramstxt = fixture_file_upload('/files/test_params.txt', 'text/xml')
     #@dataother = fixture_file_upload('/files/test_data.txt', 'text/txt')
     #@paramsother = fixture_file_upload('/files/test_params.bad.txt', 'text/txt')
   end
@@ -17,29 +17,29 @@ describe GeneralController do
       # flash[:notice].should == "test_data.xml was successfully uploaded."
     end
 
-  #  it "should not upload a non xml file" do
+    it "should not upload a non xml file" do
   #    MassData.stub(:create!).with({:file => @dataother})
-  #    post :uploadData, :upload => @dataother
-  #    response.should redirect_to new_mass_datum_path
+      post :uploadData, :upload => @dataother
+      response.should redirect_to new_mass_datum_path
   #    flash[:notice].should == "test_data.txt is not a xml file."
-  #  end
+    end
   end
 
 	describe 'upload a param data' do
     it "can upload a valid mass param file" do
-      # fake_data = mock('MassParams', :title => 'test_params.xml')
+      # fake_data = mock('MassParams', :title => 'test_params.txt')
       # MassParams.stub(:create!).with({:file => @paramsxml}).and_return(fake_data)
-      post :upload, :param_file => @paramsxml
+      post :upload, :param_file => @paramstxt
       response.should redirect_to review_path
       # flash[:notice].should == "test_data.xml and test_params.xml was successfully uploaded."
     end
 
-#    it "should not upload a non xml file" do
+    it "should not upload a non xml file" do
 #      MassParams.stub(:create!).with({:file => @paramsother})
-#      post :uploadParams, :upload => @paramsother
-#      response.should redirect_to new_mass_param_path
+      post :uploadParams, :upload => @paramsother
+      response.should redirect_to new_mass_param_path
 #      flash[:notice].should == "test_params.bad.txt is not a valid data file."
-#    end
+    end
   end
 
   describe "GET 'index'" do
