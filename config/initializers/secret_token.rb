af8d8e2dfc25899e6562126af28acfcd28e18f85
Shaina <skrevat@berkeley.edu>
2015-04-03 16:10:32 -0300

    heroku secret changes

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MassSpec::Application.config.secret_token = '61945de01e3aeac14449386b274806965b64e029afa5b9b760f9cffd73088344ba958fc7b66465ea7f7eaf90c3c0ede0f65edc1149fef73d0b1ac364f6ed2e7f'
+MassSpec::Application.config.secret_token =if Rails.env.development? or Rails.env.test?
+  ('x' * 30) # meets minimum requirement of 30 chars long
+else
+  ENV['SECRET_TOKEN']
+end
