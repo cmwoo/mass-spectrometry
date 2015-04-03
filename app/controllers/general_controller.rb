db47c99f4e9aa3d465adf5aa3228fe72364e2ce2
Chloe Lischinsky <chloeel@berkeley.edu>
2015-04-03 16:44:38 -0700

    cucumber pssin

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -7,12 +7,6 @@ class GeneralController < ApplicationController
   end
 
   def upload
-    if params[:param_file].nil?
-      flash[:warning] = "No file input."
-      redirect_to new_mass_param_path
-    else
-      redirect_to review_path
-    end
   end
 
   def examples
