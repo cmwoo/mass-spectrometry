5524367ee41ac0ca04ddc92a78a1d3dfa1890282
Shaina <skrevat@berkeley.edu>
2015-04-09 22:52:26 -0300

    user data model additions

--- app/controllers/mass_data_controller.rb
+++ app/controllers/mass_data_controller.rb
@@ -6,17 +6,24 @@ class MassDataController < ApplicationController
       redirect_to new_mass_datum_path
     else
       mass_datum = MassDatum.create(:s3id => params[:s3_key], :user_id => current_user.id)
-    if current_user.current_result
-      result = current_user.current_result
-      result.mass_data_id = mass_datum.id
-    else
-      result = Result.create(:mass_data_id => mass_datum.id, :user_id => current_user.id, :flag => false)
-    end
+      current_result = current_user.current_result
+      if current_result
+        current_result.mass_data_id = mass_datum.id
+        current_result.save
+      else
+        Result.create(:mass_data_id => mass_datum.id, :user_id => current_user.id, :flag => false)
+      end
       redirect_to new_mass_param_path
     end
   end
 
   def new
+    current_result = current_user.current_result
+    if current_result && current_result.mass_data
+      @message = "You have already uploaded #{current_result.mass_data.s3id}."
+    else
+      @message = "Please choose a .zxml file to upload."
+    end
     @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: 201)
     @mass_datum = MassDatum.new
   end
--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,15 +2,27 @@ class ResultsController < ApplicationController
   before_filter :authenticate_user!
   def index
     # final results
-    current_user.current_result.set_as_run
+    if !current_user.current_result
+      #error
+      flash[:warning] = "Please upload files to run."
+    elsif
+      if !current_user.current_result.mass_params_id || !current_user.current_result.mass_data_id
+        #error
+        flash[:warning] = "Please upload files to run."
+      else
+        current_user.current_result.set_as_run
+      end
+    end
   end
 
   def review
    # "review and run"
    if !current_user.current_result
+     flash[:warning] = "You've already run these files."
      @disabled = true
    elsif
      if !current_user.current_result.mass_params_id || !current_user.current_result.mass_data_id
+       flash[:warning] = "You haven't uploaded one of the files."
        @disabled = true
      end
    end
