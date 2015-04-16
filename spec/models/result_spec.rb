dcf88d33a68cce98e194d812ab94064ad66edd6b
Ava Dordi <ava@dordi.us>
2015-04-16 16:43:58 -0300

    added rspec test

--- spec/models/result_spec.rb
+++ spec/models/result_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Result do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "SSH Access" do
+    
+    it "should send correct commands to the connection" do
+      ssh_connection = mock("SSH Connection")
+      Net::SSH.stub(:start).and_yield(ssh_connection)
+      ssh_connection.should_receive(:exec!).with("echo 'Graph search is successfully running. You will receive an email when your analysis is complete.'")
+      Result.start_ssh
+    end
+  end
 end
