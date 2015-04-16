b641da394f315707179bc10d6adb34f77d45f91b
saasbook <saasbook@saasbook.(none)>
2015-04-16 15:54:14 -0300

    choose params

--- app/controllers/mass_params_controller.rb
+++ app/controllers/mass_params_controller.rb
@@ -19,6 +19,7 @@ class MassParamsController < ApplicationController
   end
 
   def new
+    @disabled_params = true
     current_result = current_user.current_result
     current_mass_params = if current_result then current_result.get_mass_params end
     if current_result && current_mass_params
@@ -30,4 +31,35 @@ class MassParamsController < ApplicationController
     @mass_param = MassParam.new
   end
 
+  def choose
+    @all_params = MassParam.where(:user_id => current_user.id)
+    @disabled_params = true
+    @results = current_user.results
+    current_result = current_user.current_result
+    current_mass_params = if current_result then current_result.get_mass_params end
+    if current_result && current_mass_params
+      @message = "You have already uploaded #{current_mass_params.get_title}."
+    else
+      @message = "Please choose a .txt or .params file to upload."
+    end
+    @s3_direct_post = S3_BUCKET.presigned_post(key: "params/#{current_user.id}/${filename}", success_action_status: 201)
+    @mass_param = MassParam.new
+  end
+
+  def update_params
+    data = MassParam.find(params[:data_id])
+    if !data
+      flash[:warning] = "Please choose an existing params file."
+      return redirect_to choose_params_path
+    end
+    current_result = current_user.current_result
+    if current_result
+      flash[:warning] = current_result.mass_params_id
+      current_result.mass_params_id = params[:data_id]
+      current_result.save
+    else
+      Result.create(:mass_params_id => params[:data_id], :user_id => current_user.id, :flag => false)
+    end
+    redirect_to review_path
+  end
 end
