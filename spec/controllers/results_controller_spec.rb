d5b1c59806187d45996976fb2679f4b0cffdf4cd
Ava Dordi <ava@dordi.us>
2015-04-17 17:50:17 -0300

    results spec

--- spec/controllers/results_controller_spec.rb
+++ spec/controllers/results_controller_spec.rb
@@ -49,4 +49,29 @@ describe ResultsController do
     end
   end
 
+  describe 'test review page' do
+    it 'should tell you if no files have been uploaded' do
+      user = double('user', :current_result => nil)
+      allow(request.env['warden']).to receive(:authenticate!) { user }
+      allow(controller).to receive(:current_user) { user }
+
+      get 'review'
+      flash[:warning].should == "No files have been uploaded."
+    end
+
+    it 'should tell you if no data has been uploaded' do
+      res = double('result')
+      user = double('user', :current_result => res)
+      allow(request.env['warden']).to receive(:authenticate!) { user }
+      allow(controller).to receive(:current_user) { user }
+
+      res.stub(:mass_params_id).and_return(1)
+      res.stub(:mass_data_id).and_return(nil)
+
+      get 'review'
+      flash[:warning].should == "One of the files has not been uploaded."
+    end
+
+  end
+
 end
