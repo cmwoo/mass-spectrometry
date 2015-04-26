231fe2ff35c9b3b8d4943a2540bbd970bb4f6925
jongrundman <jonathangrundman@Jonathans-MacBook-Pro.local>
2015-04-25 20:28:12 -0700

    Fixing links Downloads page

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -1,12 +1,14 @@
 require 'net/ssh'
 
 class GeneralController < ApplicationController
+  before_filter :authenticate_user!, only: [:downloads]
+  
   @@file_downloads = ["CWT_PD.exe", 
                       "Graph_search.exe",
                       "ExampleParams.params",
                       "Tag_finder.exe",
                       "ExampleParams.txt"]
-                      
+
   @@downloads_path = File.join(Rails.root, "public", "downloads_page")
 
   def index
@@ -29,6 +31,9 @@ class GeneralController < ApplicationController
       flash[:warning] = "Invalid filename"
       redirect_to downloads_path
     end
+  end
+
+  def downloads
   end
 
   def about
